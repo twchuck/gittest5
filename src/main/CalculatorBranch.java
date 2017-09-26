5fd06597b90cc847d25d45666ca417b15b3e6185
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-26 09:54:58 +0800

    remove branch sub

--- src/main/CalculatorBranch.java
+++ src/main/CalculatorBranch.java
@@ -3,10 +3,6 @@ package main;
 public class CalculatorBranch {
 
 
-	public static int sub(int a, int b) {
-		return a - b - b - b - b;
-	}
-
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
