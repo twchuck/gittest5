dd31c91da954f44530188ea0c8df7daafda7d4a2
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-26 10:01:56 +0800

    add master add2

--- src/main/CalculatorMaster.java
+++ src/main/CalculatorMaster.java
@@ -6,11 +6,15 @@ public class CalculatorMaster {
 		return a + b + b + b;
 	}
 
+	public static int add2(int a, int b) {
+		return a + b;
+	}
+
 	public static int sub(int a, int b) {
 		return a - b - b - b - b;
 	}
 
 	public static int sub2(int a, int b) {
-		return a - b ;
+		return a - b;
 	}
 }
