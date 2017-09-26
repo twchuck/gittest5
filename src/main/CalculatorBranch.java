cf48a5e2d14bab8f7ba46d1c7d13dfbde481383a
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-26 10:02:48 +0800

    add branch div2

--- src/main/CalculatorBranch.java
+++ src/main/CalculatorBranch.java
@@ -2,12 +2,15 @@ package main;
 
 public class CalculatorBranch {
 
-
 	public static int multiply(int a, int b) {
 		return a * b * b;
 	}
 
 	public static int div(int a, int b) {
 		return a / b / b;
+	}
+
+	public static int div2(int a, int b) {
+		return a / b;
 	}
 }
