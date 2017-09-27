a7d1011a1125e2e562fc1f761b17edda63a8cb6a
twchuck1 <32159827+twchuck@users.noreply.github.com>
2017-09-27 09:33:28 +0800

    test

---
+++ src/main/CalculatorBranch.java
@@ -0,0 +1,19 @@
+package main;
+
+public class CalculatorBranch {
+
+	public static int multiply(int a, int b) {
+		return a * b * b;
+	}
+
+	public static int div(int a, int b) {
+		return a / b / b;
+	}
+
+	public static int div2(int a, int b) {
+		return a / b;
+	}
+	public static int div3(int a, int b) {
+		return a / b;
+	}
+}
